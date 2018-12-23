d7e31596069a4018b84ce8a3675a62cef6d40f88
Dale Halling <hrangar@protonmail.com>
2018-12-23 16:35:33 -0600

    working on Promises - not working - not request message

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { PermissionsAndroid } from 'react-native';
 
 import FetchLocation from './components/FetchLocation';
 
 export default class App extends React.Component {
+
+async requestLocationPermission(){
+  const chckLocationPermission = PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+    if (chckLocationPermission === PermissionsAndroid.RESULTS.GRANTED) {
+        alert("You've access for the location");
+    } else {
+      console.log(chckLocationPermission);
+      try {
+        console.log('testing');
+          const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+              {
+                  'title': 'Cool Location App required Location permission',
+                  'message': 'We required Location permission in order to get device location ' +
+                      'Please grant us.'
+              }
+          );
+          console.log(granted);
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+              alert("You've access for the location");
+          } else {
+              alert("You don't have access for the location");
+          }
+      } catch (err) {
+          alert(err);
+      }
+    }
+}
+    
   getUserLocationHandler = () => {
-    console.log('Pressed the button');
+    
+    console.log('awaitBefore');
+    this.requestLocationPermission();
+    console.log('awaitAfter');
+    
+    console.log("Pressed");
+    if(navigator.geolocation) {
+      console.log("navigator.geolocation is available");
+      navigator.geolocation.getCurrentPosition(function(position) {
+        console.log("current position acquired: ", position);
+      });  
+    }
+     navigator.geolocation.getCurrentPosition(position => {
+       console.log(position);
+     }, err => console.log(err));
   }
   
   render() {
